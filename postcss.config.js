20ba9a4448a7eb7f1ab54adf31b96e748ed7c58e
JiangYingjin <i@JYJ.cx>
2025-08-30 15:39:04 +0800

    新增 autoprefixer 和 postcss 依赖，更新相关版本以提升样式处理能力和兼容性，同时更新 yarn.lock 文件以反映依赖变化。

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      // 配置 autoprefixer 选项
+      grid: true,
+      // 支持的浏览器版本
+      overrideBrowserslist: [
+        '> 0.5%',
+        'last 2 versions',
+        'not dead',
+        'not ie 11',
+        'iOS >= 10',
+        'Android >= 7'
+      ],
+      // 禁用某些警告
+      flexbox: 'no-2009',
+      // 处理 end 值兼容性
+      cascade: false
+    },
+    // 如果需要其他 PostCSS 插件可以在这里添加
+    // 'postcss-flexbugs-fixes': {},
+  },
+}
